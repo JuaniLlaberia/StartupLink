a81089b6c64581b0f9d83e2a0f578f6ecebaac5a
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-15 23:52:32 -0300

    Create enum's labels

---
+++ src/lib/labels.ts
@@ -0,0 +1,32 @@
+export const INDUSTRY_LABELS: Record<string, string> = {
+  TECH_SOFTWARE: 'Tech & Software',
+  FINANCE: 'Finance & Economy',
+  HEALTHCARE: 'Healthcare',
+  EDUCATION: 'Education',
+  SUSTAINABILITY: 'Sustainability',
+  ECOMMERCE: 'E-Commerce & Retail',
+  ENTERTAINMENT: 'Entertainment',
+  TRANSPORTATION: 'Transportation',
+  FOOD: 'Food & Beverage',
+  REAL_ESTATE: 'Real Estate',
+  PRODUCTIVITY: 'Productivity',
+  SOCIAL: 'Social & Comminuty',
+};
+
+export const STAGE_LABELS: Record<string, string> = {
+  IDEA: 'Idea Stage',
+  MVP: 'MVP Stage',
+  PRE_SEED: 'Pre Seed Stage',
+  SEED: 'Seed Stage',
+  EARLY_STAGE: 'Early Stage',
+  LATE_STAGE: 'Late Stages',
+  GROWTH: 'Growth Stage',
+  EXIT: 'Exited',
+};
+
+export const TEAM_SIZE_LABELS: Record<string, string> = {
+  ONE_TO_TEN: '1-10',
+  TEN_TO_FIFTY: '10-50',
+  FIFTY_TO_NINETY_NINE: '50-99',
+  ONE_HUNDRED_OR_MORE: '100 or more',
+};
