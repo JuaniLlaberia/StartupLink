eb406536c952d319fa27a078e8fd8127159da4be
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-18 17:26:21 -0300

    Create extra pages for landing page

---
+++ src/app/(landing-routes)/about/page.tsx
@@ -0,0 +1,34 @@
+const AboutPage = () => {
+  return (
+    <div className='p-1 py-14 space-y-10'>
+      <div className='space-y-3'>
+        <h1 className='text-6xl font-bold'>
+          Find your startup.
+          <br /> Meet your partners.
+        </h1>
+        <h2 className='text-2xl max-w-3xl font-medium text-muted-foreground'>
+          We help startups connect with co-founders, team members, and other
+          startups, making the process simple and efficient so you can focus on
+          building and growing your vision.
+        </h2>
+      </div>
+      <p className='text-lg max-w-3xl'>
+        Our mission is to connect startups with the right people and
+        opportunities. Whether you&apos;re searching for a co-founder, team
+        members, or partnerships with other startups, we make the process simple
+        and efficient. <br /> Instead of spending endless hours networking, our
+        platform helps you browse, match, and collaborate effortlessly. We
+        believe that innovation thrives when startups have access to the right
+        talent and resources, so we&apos;re here to make those connections
+        easier. With smart search tools and seamless interactions, we take the
+        complexity out of finding the right people—so you can focus on building,
+        launching, and growing your startup.
+      </p>
+      <h3 className='text-2xl font-semibold'>
+        Your startup journey starts here 🚀
+      </h3>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/(landing-routes)/privacy/page.tsx
@@ -0,0 +1,123 @@
+const PrivacyPolicyPage = () => {
+  return (
+    <div className='flex justify-center'>
+      <div className='flex flex-col max-w-xl gap-4 my-10 px-4'>
+        <h2 className='font-bold text-lg'>[APP NAME] Privacy Policy</h2>
+        <>
+          This Privacy Policy describes how [APP NAME] (&quot;we&quot;,
+          &quot;our&quot;, or &quot;us&quot;) collects, uses, and shares
+          information when you use our website located at [APP NAME].app (the
+          &quot;Website&quot;) and the services offered by [APP NAME] (the
+          &quot;Service&quot;).
+        </>
+        <p>
+          <strong>1. Information We Collect</strong>
+        </p>
+        <p>
+          We collect certain information when you use our Service, including:
+        </p>
+        <ul>
+          <li>
+            Information you provide to us, such as when you create an account,
+            submit user content, or contact us for support.
+          </li>
+          <li>
+            Automatically collected information, such as your IP address, device
+            information, and usage data when you access or use our Service.
+          </li>
+        </ul>
+        <p>
+          <strong>2. How We Use Your Information</strong>
+        </p>
+        <p>
+          We use the information we collect for various purposes, including:
+        </p>
+        <ul>
+          <li>Providing, maintaining, and improving our Service.</li>
+          <li>
+            Communicating with you, including responding to your inquiries and
+            providing customer support.
+          </li>
+          <li>
+            Complying with legal obligations and enforcing our terms and
+            policies.
+          </li>
+        </ul>
+        <p>
+          <strong>3. Information Sharing</strong>
+        </p>
+        <p>
+          We may share your information with third parties for various purposes
+          such as:
+        </p>
+        <ul>
+          <li>
+            Service providers who assist us in operating our Service or
+            conducting our business.
+          </li>
+          <li>
+            Third parties with whom you authorize us to share your information.
+          </li>
+          <li>
+            Law enforcement or government agencies in response to legal requests
+            or as necessary for legal compliance.
+          </li>
+          <li>
+            Other parties in connection with a corporate transaction, such as a
+            merger or acquisition.
+          </li>
+        </ul>
+        <p>
+          <strong>4. Your Choices</strong>
+        </p>
+        <p>
+          You have certain choices regarding the information we collect and how
+          it&apos;s used, including:
+        </p>
+        <ul>
+          <li>
+            Updating or correcting your information by accessing your account
+            settings.
+          </li>
+          <li>
+            Opting out of certain data collection and sharing by adjusting your
+            browser settings.
+          </li>
+          <li>You can delete your data at anytime.</li>
+          <li>
+            Contacting us if you have any questions or concerns about your
+            privacy.
+          </li>
+        </ul>
+        <p>
+          <strong>5. Data Security</strong>
+        </p>
+        <p>
+          We take reasonable measures to protect your information from
+          unauthorized access, disclosure, alteration, or destruction.
+        </p>
+        <p>
+          <strong>6. Changes to This Policy</strong>
+        </p>
+        <p>
+          We may update this Privacy Policy from time to time. We will notify
+          you of any changes by posting the new Privacy Policy on the Website.
+          You are advised to review this Privacy Policy periodically for any
+          changes.
+        </p>
+        <p>
+          <strong>7. Contact Us</strong>
+        </p>
+        <p>
+          If you have any questions about this Privacy Policy, please contact us
+          at [INSERT EMAIL].
+        </p>
+        <p className='mt-4 text-center text-sm text-muted-foreground'>
+          Last updated: February 18, 2025
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PrivacyPolicyPage;
