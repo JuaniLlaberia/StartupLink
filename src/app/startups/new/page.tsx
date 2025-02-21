a48597fea27ffd5b69c3b54a97cfcdb159e374d3
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-21 16:49:51 -0300

    Add auth check

--- src/app/startups/new/page.tsx
+++ src/app/startups/new/page.tsx
@@ -1,6 +1,12 @@
+import { redirect } from 'next/navigation';
+
+import { auth } from '@/auth';
 import StartupForm from './(components)/startup-form';
 
-const NewStartupPage = () => {
+const NewStartupPage = async () => {
+  const session = await auth();
+  if (!session) redirect('/');
+
   return (
     <section className='p-1 py-8'>
       <div className='min-h-[75dvh] flex items-center justify-center'>
