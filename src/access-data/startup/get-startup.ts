3f2b79f37fe966c29b5be91fef64f480463e0413
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-24 22:48:44 -0300

    Update funciton to get startup by id

--- src/access-data/startup/get-startup.ts
+++ src/access-data/startup/get-startup.ts
@@ -1,12 +1,24 @@
 'server only';
 
+import { redirect } from 'next/navigation';
+
+import { getAuthUser } from '@/actions/auth';
 import { db } from '@/db';
+import { isStartupMember } from '../helper';
 
 type getStartupProps = {
   startupId: string;
 };
 
 export const getStartup = async ({ startupId }: getStartupProps) => {
+  const userId = await getAuthUser();
+  const isMember = await isStartupMember(startupId, userId);
+
+  if (!isMember) redirect('/startups');
+
   const startup = await db.startup.findUnique({ where: { id: startupId } });
+
+  if (!startup) redirect('/my-startups');
+
   return startup;
 };
