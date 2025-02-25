1187248e22ba57e417563fdb061cca106e6c58eb
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-24 22:45:34 -0300

    Update logo

--- src/components/custom/logo.tsx
+++ src/components/custom/logo.tsx
@@ -1,13 +1,29 @@
 import Link from 'next/link';
 import { Infinity } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
-const Logo = () => {
+const Logo = ({ size = 'default' }: { size?: 'sm' | 'default' }) => {
   return (
     <Link href='/' className='flex items-center gap-2.5'>
-      <div className='bg-violet-500 p-1.5 rounded-lg text-white'>
-        <Infinity className='size-6' strokeWidth={2.25} />
+      <div
+        className={cn(
+          'bg-violet-500 rounded-lg text-white',
+          size === 'default' ? 'p-1.5' : 'p-1'
+        )}
+      >
+        <Infinity
+          className={cn(size === 'default' ? 'size-6' : 'size-5')}
+          strokeWidth={2.25}
+        />
       </div>
-      <h1 className='font-bold text-xl hidden md:block'>StartupLink</h1>
+      <h1
+        className={cn(
+          'font-bold hidden md:block group-data-[collapsible=icon]:hidden',
+          size === 'default' ? 'text-xl' : 'text-lg'
+        )}
+      >
+        StartupLink
+      </h1>
     </Link>
   );
 };
