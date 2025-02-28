34c47067f67049498d941e05ad9bf98ce22afd39
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-28 17:10:11 -0300

    Implement function to query startups by slug

---
+++ src/access-data/startup/get-startup-by-slug.ts
@@ -0,0 +1,59 @@
+'server only';
+
+import { redirect } from 'next/navigation';
+
+import { db } from '@/db';
+
+type getStartupBySlugProps = {
+  startupSlug: string;
+};
+
+export const getStartupBySlug = async ({
+  startupSlug,
+}: getStartupBySlugProps) => {
+  const startup = await db.startup.findUnique({
+    where: { slug: startupSlug },
+    include: {
+      StartupMember: {
+        include: {
+          user: {
+            select: {
+              name: true,
+              image: true,
+            },
+          },
+          role: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+      StartupDesignConfig: true,
+      Upvote: {
+        select: { id: true },
+      },
+    },
+  });
+
+  if (!startup) redirect('/');
+
+  const upvotesCount = startup.Upvote.length;
+
+  const formattedStartup = {
+    ...startup,
+    upvotesCount,
+    members: startup.StartupMember.map(member => ({
+      id: member.id,
+      name: member.user.name,
+      image: member.user.image,
+      role: member.role.name,
+    })),
+    designConfig: startup.StartupDesignConfig[0] || null,
+    StartupMember: undefined,
+    Upvote: undefined,
+    StartupDesignConfig: undefined,
+  };
+
+  return formattedStartup;
+};
