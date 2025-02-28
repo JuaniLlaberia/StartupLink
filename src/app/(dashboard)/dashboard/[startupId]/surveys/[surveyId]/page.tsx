2780e9faffcdec430c83a46e3df294eb82b003af
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-02-28 15:33:57 -0300

    Create surveys ui components

---
+++ src/app/(dashboard)/dashboard/[startupId]/surveys/[surveyId]/page.tsx
@@ -0,0 +1,19 @@
+import { getSurvey } from '@/access-data/survey/get-survey';
+import SurveyForm from '../new/(components)/survey-form';
+
+const SurveyPage = async ({
+  params,
+}: {
+  params: Promise<{ startupId: string; surveyId: string }>;
+}) => {
+  const { startupId, surveyId } = await params;
+  const survey = await getSurvey({ surveyId, startupId });
+
+  return (
+    <section>
+      <SurveyForm startupId={startupId} data={survey} />
+    </section>
+  );
+};
+
+export default SurveyPage;
