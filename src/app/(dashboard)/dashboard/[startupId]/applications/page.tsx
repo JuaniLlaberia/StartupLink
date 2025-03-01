479d21a2331eb3c75f26f73ddfd6433558720b28
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-03-01 00:10:01 -0300

    Fix landing page layout

---
+++ src/app/(dashboard)/dashboard/[startupId]/applications/page.tsx
@@ -0,0 +1,11 @@
+const ApplicationsPage = () => {
+  return (
+    <section>
+      <header className='space-y-1 mb-2'>
+        <h1 className='text-lg font-medium'>Applications</h1>
+      </header>
+    </section>
+  );
+};
+
+export default ApplicationsPage;
---
+++ src/app/(dashboard)/dashboard/[startupId]/events/page.tsx
@@ -0,0 +1,11 @@
+const EventsPage = () => {
+  return (
+    <section>
+      <header className='space-y-1 mb-2'>
+        <h1 className='text-lg font-medium'>Events</h1>
+      </header>
+    </section>
+  );
+};
+
+export default EventsPage;
