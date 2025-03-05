1df7e112468cd11904caa39289e7fa1f4bcce5ee
JuaniLlaberia <juanillaberia2002@gmail.com>
2025-03-05 16:47:42 -0300

    Remove components

--- src/app/(dashboard)/dashboard/[startupId]/(components)/dashboard-navbar.tsx
+++ src/app/(dashboard)/dashboard/[startupId]/(components)/dashboard-navbar.tsx
@@ -1,4 +1,3 @@
-import NotificationsMenu from '@/components/custom/notifications-menu';
 import UserMenu from '@/components/custom/user-menu';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 
@@ -7,7 +6,6 @@ const DashboardNavbar = () => {
     <nav className='sticky top-0 z-50 bg-background w-full flex items-center justify-between gap-4 p-2'>
       <SidebarTrigger />
       <div className='flex items-center gap-2.5'>
-        <NotificationsMenu />
         <UserMenu />
       </div>
     </nav>
